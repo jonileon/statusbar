88e38533e1629f4b1a8565aad5498fad06a21da1
Joni <jonathan@trebels.com>
2026-08-14 17:26:56 +0200

    added main loop and formatting structure, as well as volume

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module statusbar
 
 go 1.26.6
+
+require (
+	github.com/go-ole/go-ole v1.3.0 // indirect
+	github.com/itchyny/volume-go v0.2.3 // indirect
+	github.com/moutend/go-wca v0.3.0 // indirect
+	golang.org/x/sys v0.47.0 // indirect
+)
--- src/main.go
+++ src/main.go
@@ -11,14 +11,19 @@ import (
 	"strconv"
 	"sync"
 	"time"
+	"github.com/itchyny/volume-go"
 )
+
+const inactive_color = "#555555"
+const active_color = "#ffffff"
+const output_format = "<b><span foreground=\"%v\">ctrl</span> <span foreground=\"%v\">shift</span> <span foreground=\"%v\">alt</span> <span foreground=\"%v\" >super</span> │ power-prof: <span foreground=\"#ffbb55\">balanced-battery</span> │ wifi: <span foreground=\"#8888dd\">HaDiFunk</span> │ %v <span foreground=\"#ff5555\">%v%%</span> │  [BAT] %v%% <span foreground=\"#ffffbd\">(%02d:%02d)</span> │ %v %v-%02d-%02d <span foreground=\"#ffff7b\">%02d:%02d:%02d</span></b>"
 
 // battery datat structures
 type Bat struct {
 	IsCharging bool
 	Charge uint8
-	Timeh uint8
-	Timem uint8
+	Timeh  uint8
+	Timem  uint8
 }
 var bat_lock = sync.Mutex{}
 var bat_data = Bat{}
@@ -27,11 +32,18 @@ var bat_data = Bat{}
 type ModKeys struct {
     Shift bool `json:"shift"`
     Ctrl  bool `json:"ctrl"`
-    Meta   bool `json:"meta"`
+    Meta  bool `json:"meta"`
     Alt   bool `json:"alt"`
 }
 var modkey_lock = sync.Mutex{}
 var modkey_data = ModKeys{}
+
+type VolumeInfo struct {
+	Muted bool
+	Volume uint8
+}
+var volume_lock = sync.Mutex{}
+var volume_data = VolumeInfo{}
 
 
 func readIntFromFs(path string) int64 {
@@ -48,9 +60,63 @@ func readIntFromFs(path string) int64 {
 
 func main() {
 	go modkey_update()
+	go bat_update()
+	go volume_update()
 	for true {
-		time := time.Date()
-				
+		now := time.Now()
+		year, month, day := now.Date()
+
+		ctrl := inactive_color
+		meta := inactive_color
+		alt := inactive_color
+		shift := inactive_color
+		modkey_lock.Lock()
+		if modkey_data.Ctrl {
+			ctrl = active_color
+		}
+		if modkey_data.Meta {
+			meta = active_color
+		}
+		if modkey_data.Alt {
+			alt = active_color
+		}
+		if modkey_data.Shift {
+			shift = active_color
+		}
+		modkey_lock.Unlock()
+
+		vol_string := "[VOL]"
+		volume_lock.Lock()
+		vol := volume_data.Volume
+		if volume_data.Muted {
+			vol_string = "[MUT]"
+		}
+		volume_lock.Unlock()
+
+		bat_lock.Lock()
+		bat := bat_data.Charge
+		h := bat_data.Timeh
+		m := bat_data.Timem
+		bat_lock.Unlock()
+		
+		output, err := fmt.Printf(output_format,
+			// modifier keys
+			ctrl, shift, alt, meta,
+			// volume
+			vol_string, vol,
+			// battery
+			bat, h, m,
+			// time date
+			now.Weekday().String(),
+			year, int(month), day,
+			now.Hour(), now.Minute(), now.Second(),
+		)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(output)
+
+		time.Sleep(500 * time.Millisecond)
 	}
 
 }
@@ -99,12 +165,31 @@ func modkey_update() {
 		err := json.Unmarshal([]byte(line), &modkey_data)
 		if err != nil {
 			log.Fatal("Something went wrong while parsing json")
-		} else {
-			fmt.Print(modkey_data)
-		}
+		}		
 		modkey_lock.Unlock()
 	}
 	if err := scanner.Err(); err != nil {
 		log.Fatal("Scan Error: ", err)	
+	}
+}
+
+func volume_update() {
+	for true {
+		muted, err := volume.GetMuted()
+		if err != nil {
+			log.Fatal(err)
+		}
+		vol, err := volume.GetVolume()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// update
+		volume_lock.Lock()
+		volume_data.Muted = muted
+		volume_data.Volume = uint8(vol)
+		volume_lock.Unlock()
+		
+		time.Sleep(500 * time.Millisecond)
 	}
 }
